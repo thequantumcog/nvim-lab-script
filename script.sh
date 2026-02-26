0454cfe7808317dee7449cc3cab2a1ff5af58dc3
Quantum Cog <thequantumcog@protonmail.com>
2026-02-26 15:24:09 +0500

    Reorder things

--- script.sh
+++ script.sh
@@ -4,8 +4,7 @@ set -e
 mkdir -p "$HOME/.local/bin"
 cd /tmp
 
-sudo apt update
-sudo apt install -y git kitty wl-clipboard curl ripgrep
+sudo apt install -y git kitty wl-clipboard ripgrep
 
 wget -q https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 tar xzf nvim-linux-x86_64.tar.gz
